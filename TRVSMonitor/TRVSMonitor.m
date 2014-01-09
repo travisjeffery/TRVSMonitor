eafa820654785a9e4c3de91e2870b9b1c5e113c6
Travis Jeffery <tj@travisjeffery.com>
2014-01-09 14:24:21 -0500

    Style

--- TRVSMonitor/TRVSMonitor.m
+++ TRVSMonitor/TRVSMonitor.m
@@ -8,71 +8,82 @@
 
 #import "TRVSMonitor.h"
 
-@implementation TRVSMonitor {
-    NSInteger _signalsRemaining;
-    NSInteger _expectedSignalCount;
-}
+@interface TRVSMonitor ()
+
+@property(nonatomic) NSInteger signalsRemaining;
+@property(nonatomic) NSInteger expectedSignalCount;
+
+@end
+
+@implementation TRVSMonitor
 
 + (instancetype)monitor {
-    return [[self alloc] init];
+  return [[self alloc] init];
 }
 
 - (instancetype)init {
-    return [self initWithExpectedSignalCount:1];
+  return [self initWithExpectedSignalCount:1];
 }
 
 - (instancetype)initWithExpectedSignalCount:(NSInteger)expectedSignalCount {
-    self = [super init];
-    if (self) {
-        _expectedSignalCount = expectedSignalCount;
-        [self reset];
-    }
-    return self;
+  self = [super init];
+  
+  if (self) {
+    self.expectedSignalCount = expectedSignalCount;
+    [self reset];
+  }
+  
+  return self;
 }
 
 - (BOOL)wait {
-    return [self waitWithTimeout:0.0];
+  return [self waitWithTimeout:0.0];
 }
 
 - (BOOL)waitWithSignalHandler:(TRVSMonitorHandler)handler {
-    return [self waitWithTimeout:0.0 signalHandler:handler];
+  return [self waitWithTimeout:0.0 signalHandler:handler];
 }
 
 - (BOOL)waitWithTimeout:(NSTimeInterval)timeout {
-    return [self waitWithTimeout:timeout signalHandler:nil];
+  return [self waitWithTimeout:timeout signalHandler:nil];
 }
 
-- (BOOL)waitWithTimeout:(NSTimeInterval)timeout signalHandler:(TRVSMonitorHandler)handler {
-    NSDate *start = [NSDate date];
+- (BOOL)waitWithTimeout:(NSTimeInterval)timeout
+          signalHandler:(TRVSMonitorHandler)handler {
+  NSDate *start = [NSDate date];
 
-    while (_signalsRemaining > 0) {
-        [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:.1]];
-        
-        if ([self didTimeOut:timeout fromStartDate:start]) {
-            [self reset];
-            return NO;
-        }
-        
-        if (handler) handler(self);
+  while (self.signalsRemaining > 0) {
+    [[NSRunLoop currentRunLoop]
+        runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+
+    if ([self didTimeOut:timeout fromStartDate:start]) {
+      [self reset];
+      
+      return NO;
     }
-    
-    [self reset];
-    
-    return YES;
+
+    if (handler)
+      handler(self);
+  }
+
+  [self reset];
+
+  return YES;
 }
 
 - (void)signal {
-    --_signalsRemaining;
-}
-
-- (void)reset {
-    _signalsRemaining = _expectedSignalCount;
+  self.signalsRemaining -= 1;
 }
 
 #pragma mark - Private
 
+- (void)reset {
+  self.signalsRemaining = self.expectedSignalCount;
+}
+
 - (BOOL)didTimeOut:(NSTimeInterval)timeout fromStartDate:(NSDate *)startDate {
-    return (timeout > 0.0 && [[NSDate date] timeIntervalSinceDate:startDate] > timeout);
+  return (timeout > 0.0 && [[NSDate date] timeIntervalSinceDate:startDate] >
+                               timeout);
 }
 
 @end
